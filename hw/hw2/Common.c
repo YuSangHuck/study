04fc7b8221b0d053e9c7e967bba6ca216ace7b9c
YuSangHuck <g01099387636@gmail.com>
2018-12-20 17:18:44 +0900

    update TCB->status

--- hw/hw2/Common.c
+++ hw/hw2/Common.c
@@ -9,6 +9,7 @@ void __thread_wait_handler(int signo){
     }
     pthread_mutex_lock(&(p_TCB->readyMutex));
     p_TCB->bRunnable = FALSE;
+    p_TCB->status = THREAD_STATUS_READY;
     while(p_TCB->bRunnable == FALSE){
         pthread_cond_wait(&(p_TCB->readyCond), &(p_TCB->readyMutex));
         // pthread_kill(p_TCB->tid, SIGSTOP);
@@ -20,6 +21,7 @@ void __thread_wakeup(Thread* ap_TCB){
     LOG_FUNC(__thread_wakeup, called);
     pthread_mutex_lock(&(ap_TCB->readyMutex));
     ap_TCB->bRunnable = TRUE;
+    ap_TCB->status = THREAD_STATUS_RUN;
     pthread_cond_signal(&(ap_TCB->readyCond));
     // pthread_kill(ap_TCB->tid, SIGCONT);
     pthread_mutex_unlock(&(ap_TCB->readyMutex));
